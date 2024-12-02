ebcb611c6a97f1652d3323d3892cb6b5d85754b8
Serpus87 <cesarvroom@gmail.com>
2024-12-02 23:06:11 +0100

    wip

--- AdventOfCode.Tests/Day2Tests.cs
+++ AdventOfCode.Tests/Day2Tests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Security;
 using System.Text;
 using System.Threading.Tasks;
 using AdventOfCode.Day2;
@@ -22,5 +23,41 @@ public class Day2Tests
 
         // Assert
         Assert.AreEqual(expectedSolution, actualSolution);
+    }
+
+    [TestMethod]
+    public void Part2Solve_Example_ReturnsExpectedSolution()
+    {
+        // Arrange
+        var expectedSolution = 4;
+        var input = InputReader.Read("Example.txt");
+
+        // Act
+        var actualSolution = Part2.Solve(input);
+
+        // Assert
+        Assert.AreEqual(expectedSolution, actualSolution);
+    }
+
+    [DataTestMethod]
+    [DataRow(4,1,2,3,true)]
+    [DataRow(1,4,2,3,true)]
+    [DataRow(1,2,4,3,true)]
+    [DataRow(2,3,4,1,true)]
+    [DataRow(4,1,3,2,false)]
+    [DataRow(2,1,4,3,false)]
+    public void Part2Solve_PrivateExample_ReturnsExpectedSolution(int level1, int level2, int level3, int level4, bool isSafe)
+    {
+        // Arrange
+        var expectedResult = isSafe ? 1 : 0;
+        var levels = new List<int> { level1, level2, level3, level4 };
+        var input = new Input();
+        input.Reports.Add(new Report(levels));
+
+        // Act
+        var result = Part2.Solve(input);
+
+        // Assert
+        Assert.AreEqual(result, expectedResult);
     }
 }
--- src/Day2/ReportExtensions.cs
+++ src/Day2/ReportExtensions.cs
@@ -9,7 +9,105 @@ namespace AdventOfCode.Day2;
 
 public static class ReportExtensions
 {
+    public static List<bool> GetSafeList(this List<Report> reports)
+    {
+        var safeList = new List<bool>();
+
+        foreach (var report in reports)
+        {
+            safeList.Add(report.IsSafe());
+        }
+
+        return safeList;
+    }
+
     public static bool IsSafe(this Report report)
+    {
+        return report.GetUnsafeLevel() == null;
+
+        //var isIncreasing = true;
+        //var isDecreasing = true;
+        //var isWithinRange = true;
+        //var isSafe = false;
+
+        //for (int i = 1; i < report.Levels.Count; i++)
+        //{
+        //    var previousLevel = report.Levels[i - 1];
+        //    var level = report.Levels[i];
+
+        //    isIncreasing = isIncreasing && level > previousLevel;
+        //    isDecreasing = isDecreasing && level < previousLevel;
+        //    isWithinRange = Math.Abs(level - previousLevel) is >= 1 and <= 3;
+
+        //    isSafe = (isIncreasing || isDecreasing) && isWithinRange;
+        //    if (!isSafe)
+        //    {
+        //        break;
+        //    }
+        //}
+
+        //return isSafe;
+    }
+
+    public static List<bool> GetSafeListWithDampner(this List<Report> reports)
+    {
+        var safeList = new List<bool>();
+
+        foreach (var report in reports)
+        {
+            safeList.Add(report.IsSafeWithDampner());
+        }
+
+        return safeList;
+    }
+
+    public static bool IsSafeWithDampner(this Report report)
+    {
+        var reportCopy = report.Copy();
+        var isSafe = false;
+        var isSafeWithoutDampner = report.IsSafe();
+
+        if (isSafeWithoutDampner)
+        {
+            return true;
+        }
+
+        var getFirstUnsafe = report.GetUnsafeLevel(true);
+        report.Levels.RemoveAt((int)getFirstUnsafe!);
+
+        isSafeWithoutDampner = report.IsSafe();
+
+        if (isSafeWithoutDampner)
+        {
+            return true;
+        }
+
+        var getSecondUnsafe = reportCopy.GetUnsafeLevel();
+        reportCopy.Levels.RemoveAt((int)getSecondUnsafe!);
+
+        isSafeWithoutDampner = reportCopy.IsSafe();
+
+        if (isSafeWithoutDampner)
+        {
+            return true;
+        }
+
+        return isSafe;
+    }
+
+    private static Report Copy(this Report originalReport)
+    {
+        var copyLevels = new List<int>();
+
+        foreach (var level in originalReport.Levels)
+        {
+            copyLevels.Add(level);
+        }
+
+        return new Report(copyLevels);
+    }
+
+    public static int? GetUnsafeLevel(this Report report, bool usePreviousLevel = false)
     {
         var isIncreasing = true;
         var isDecreasing = true;
@@ -23,27 +121,15 @@ public static class ReportExtensions
 
             isIncreasing = isIncreasing && level > previousLevel;
             isDecreasing = isDecreasing && level < previousLevel;
-            isWithinRange = Math.Abs(level - previousLevel) is >= 1 and <=3;
+            isWithinRange = Math.Abs(level - previousLevel) is >= 1 and <= 3;
 
             isSafe = (isIncreasing || isDecreasing) && isWithinRange;
             if (!isSafe)
             {
-                break;
+                return usePreviousLevel ? i - 1 : i;
             }
         }
 
-        return isSafe;
-    }
-
-    public static List<bool> GetSafeList(this List<Report> reports)
-    {
-        var safeList = new List<bool>();
-
-        foreach (var report in reports)
-        {
-            safeList.Add(report.IsSafe());
-        }
-
-        return safeList;
+        return null;
     }
 }

0532a0e2605dcfc0eda9b238c2ceb423d51650f3
Serpus87 <cesarvroom@gmail.com>
2024-12-28 15:26:47 +0100

    wip

---
+++ src/Day17/Instructions/Adv.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Day17.Instructions
+{
+    internal static class Adv
+    {
+        public static int Opcode { get; set; } = 0;
+
+        public static int Perform(int numerator, int denominator) 
+        { 
+            return numerator / denominator;
+        }
+    }
+}
